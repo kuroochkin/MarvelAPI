2fb1feea50d4d44c458e802f138d0cb831b79288
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-05-22 21:22:43 +0300

    Оптимизация и трансформация данных

--- marvelapi/src/services/MarvelService.js
+++ marvelapi/src/services/MarvelService.js
@@ -15,12 +15,24 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apikey}`); 
+    getAllCharacters = async () => {
+       const res = this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apikey}`); 
+       return res.data.results.map(this._transformCharacter);
     }
 
-    getCharacter = (id) => {
-        return this.getResource(`${this._apiBase}characters/${id}?${this._apikey}`); 
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apikey}`); 
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description,
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
     }
 }
 
