563defe43df4e11365acdbbfb532d3af18983814
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-05-23 16:11:41 +0300

    Добавления спиннера

--- marvelapi/src/components/randomChar/RandomChar.js
+++ marvelapi/src/components/randomChar/RandomChar.js
@@ -3,6 +3,7 @@ import { Component } from 'react';
 import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
 import MarvelService from '../../services/MarvelService';
+import Spinner from '../spinner/spinner';
 
 class RandomChar extends Component{
     constructor(props) {
@@ -11,43 +12,32 @@ class RandomChar extends Component{
     }
 
     state = {
-        char: {}
+        char: {},
+        loading: true
     }
 
     marvelService = new MarvelService();
 
-    onChatLoaded = (char) => {
-        this.setState({char})
+    onCharLoaded = (char) => {
+        this.setState(
+            {char, 
+            loading : false
+        })
     }
 
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
         this.marvelService
             .getCharacter(id)
-            .then(this.onChatLoaded)
+            .then(this.onCharLoaded)
     }
 
     render() {
-        const {char: {name, description, thumbnail, homepage, wiki}} = this.state;
+        const {char, loading} = this.state;
+
         return (
             <div className="randomchar">
-                <div className="randomchar__block">
-                    <img src={thumbnail} alt="Random character" className="randomchar__img"/>
-                    <div className="randomchar__info">
-                        <p className="randomchar__name">{name}</p>
-                        <p className="randomchar__descr">
-                            {description}
-                        </p>
-                        <div className="randomchar__btns">
-                            <a href={homepage} className="button button__main">
-                                <div className="inner">homepage</div>
-                            </a>
-                            <a href={wiki} className="button button__secondary">
-                                <div className="inner">Wiki</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                {loading ? <Spinner/> : <View char={char}/>}
                 <div className="randomchar__static">
                     <p className="randomchar__title">
                         Random character for today!<br/>
@@ -64,6 +54,31 @@ class RandomChar extends Component{
             </div>
         )
     }
+}
+
+const View = ({char}) => {
+
+    const {name, description, thumbnail, homepage, wiki} = char;
+
+    return (
+        <div className="randomchar__block">
+                    <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+                    <div className="randomchar__info">
+                        <p className="randomchar__name">{name}</p>
+                        <p className="randomchar__descr">
+                            {description}
+                        </p>
+                        <div className="randomchar__btns">
+                            <a href={homepage} className="button button__main">
+                                <div className="inner">homepage</div>
+                            </a>
+                            <a href={wiki} className="button button__secondary">
+                                <div className="inner">Wiki</div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+    )
 }
 
 export default RandomChar;
--- marvelapi/src/services/MarvelService.js
+++ marvelapi/src/services/MarvelService.js
@@ -28,7 +28,7 @@ class MarvelService {
     _transformCharacter = (char) => {
         return {
             name: char.name,
-            description: char.description,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'Данных о персонаже нет',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
             wiki: char.urls[1].url
