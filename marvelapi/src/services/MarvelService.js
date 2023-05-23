16aad42f302461a4e64b4743009903293923fb14
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-05-23 23:01:21 +0300

    Реализация информации о герое

--- marvelapi/src/services/MarvelService.js
+++ marvelapi/src/services/MarvelService.js
@@ -30,7 +30,8 @@ class MarvelService {
             description: char.description ? `${char.description.slice(0, 210)}...` : 'Данных о персонаже нет',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
-            wiki: char.urls[1].url
+            wiki: char.urls[1].url,
+            comics: char.comics.items
         }
     }
 }
